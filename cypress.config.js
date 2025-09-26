2bf9dc5dbf592ad39447aa23fb9cebde753d7702
Diogo Felde <diogofelde@gmail.com>
2025-09-26 00:17:01 -0300

    🧹 Remove BOM e valida JSON (package.json, vercel.json, tsconfig)

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-﻿const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:5173',
-    supportFile: false,
-    specPattern: 'cypress/e2e/**/*.cy.js'
-  }
-});
